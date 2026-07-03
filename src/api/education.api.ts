413cb14f3f563c24dd5a5e4ac482a4bdd25b8fc4
vanchay01 <chaydouble0@gmail.com>
2026-07-03 19:23:48 +0700

    test-dev branch

--- src/api/education.api.ts
+++ src/api/education.api.ts
@@ -1,22 +1,27 @@
 import { apiURL } from "./apiURL";
 
 export const educationAPI = {
+  // add education ----------------------------------------------------------------
   async addEducation(data: FormData) {
     return await apiURL.post('/education', data);
   },
 
+  // get education ----------------------------------------------------------------
   async getEducation() {
     return await apiURL.get(`/education`);
   },
 
+  // get education by ID ----------------------------------------------------------
   async getEducationById(id: number) {
     return await apiURL.get(`/education/${id}`);
   },
 
+  // update education -------------------------------------------------------------
   async updateEducation(id: number, data: FormData) {
     return await apiURL.patch(`/education/${id}`, data);
   },
 
+  // delete education -------------------------------------------------------------
   async deleteEducation(id: number) {
     return await apiURL.delete(`/education/${id}`);
   },
