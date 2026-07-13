35b109ce5ed456474e405d03e658fb6915df33e2
vanchay21s <okpro21333@gmail.com>
2026-07-13 18:25:56 +0700

    src/service/skillService.ts

--- src/services/skillService.ts
+++ src/services/skillService.ts
@@ -1,7 +1,43 @@
 import { skillAPI } from "../api/skill.api";
-
-export const getSkill = async () => {
-  return await skillAPI.getSkill();
-};
+import type { skillFrom } from "../types/skill";
 
 
+export const skillService = {
+
+  // create -------------------------------------------------------
+  create(form: skillFrom){
+    const formData = new FormData()
+    Object.entries(form).forEach(([key, value]) => {
+      if(value !== undefined && value !== null){
+        formData.append(key, value as any)
+      }
+    })
+    return skillAPI.create(formData)
+  },
+
+  // find -------------------------------------------------------
+  find(){
+    return skillAPI.find()
+  },
+
+  // find one -------------------------------------------------------
+  findOne(id: number){
+    return skillAPI.findOne(id)
+  },
+
+  // update one -------------------------------------------------------
+  updateOne(id: number, form: skillFrom){
+    const formData = new FormData()
+    Object.entries(form).forEach(([key, value]) => {
+      if(value !== undefined && value !== null){
+        formData.append(key, value as any)
+      }
+    })
+    return skillAPI.updateOne(id, formData)
+  },
+
+  // delete one -------------------------------------------------------
+  deleteOne(id: number){
+    return skillAPI.deleteOne(id)
+  }
+}
